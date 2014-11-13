f3968d1202c89e0eb7149b191e9a1f97c88a1e76
Ik-soo Kim <iksoo_kim@tmax.co.kr>
2014-11-13 17:15:48 +0900

    init project

---
+++ src/binary/tree/BTNode.java
@@ -0,0 +1,117 @@
+package binary.tree;
+
+import java.util.ArrayList;
+
+public class BTNode {
+	private BTNode parent;
+	private ArrayList<BTNode> leafs = new ArrayList<BTNode>();
+	private boolean isRoot;
+	private String id;
+
+	public BTNode(String id){
+		this.id = id;
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public boolean isRoot() {
+		return isRoot;
+	}
+
+	public void setRoot(boolean isRoot) {
+		this.isRoot = isRoot;
+	}
+
+	public void addNewLeaf(BTNode node) {
+		if (leafs.size() < 2){
+			node.setParent(this);
+			leafs.add(node);
+		}
+		else
+			new Exception("No space for leaf");
+	}
+	
+	public BTNode addNewLeaf(String id) {
+		
+		if (leafs.size() < 2){
+			BTNode leaf = new BTNode(id);
+			leaf.setParent(this);
+			leafs.add(leaf);
+			return leaf;
+		}
+		else
+			new Exception("No space for leaf");
+		return null;
+	}
+
+	public ArrayList<BTNode> getLeafs() {
+		return leafs;
+	}
+
+	public BTNode getLeaf(int index) {
+		return leafs.get(index);
+	}
+
+	public BTNode getParent() {
+		return parent;
+	}
+
+	public void setParent(BTNode parent) {
+		this.parent = parent;
+	}
+
+	public void leafToParent(String id) {
+		for (BTNode leaf : leafs) {
+			if (leaf.getId().equalsIgnoreCase(id)) {
+				if (isRoot) {
+					parent = leaf;
+					isRoot = false;
+					leafs.remove(leaf);
+					return;
+				} else {
+					leafs.remove(leaf);
+					parent.leafToParent(this.id);
+					leafs.add(parent);
+					parent = leaf;
+					continue;
+				}
+			}
+		}
+	}
+
+	public void setAsRoot() throws Exception {
+		if (!leafs.isEmpty() || leafs.size() > 0)
+			throw new Exception("I'm not leaf");
+		parent.leafToParent(id);
+		addNewLeaf(parent);
+		parent = null;
+		isRoot = true;
+	}
+
+	public void printTree() {
+		System.out.println("parent : "+parent+", id : "+this+", leafs : ["+leafs+"]");
+		if(leafs!=null){
+			for(BTNode leaf:leafs){
+				leaf.printTree();
+			}
+		}
+			
+	}
+	
+	@Override
+	public String toString() {
+		return "["+id+"]";
+	}
+
+	public void setIsRoot(boolean b) {
+		this.isRoot = b;
+		
+	}
+
+}
---
+++ src/binary/tree/BinaryTree.java
@@ -0,0 +1,27 @@
+package binary.tree;
+
+
+public class BinaryTree {
+	public static void main(String[] args) throws Exception {
+		BTNode root = new BTNode("root");
+		root.setIsRoot(true);
+		BTNode a = root.addNewLeaf("a");
+		BTNode b = root.addNewLeaf("b");
+		BTNode c = a.addNewLeaf("c");
+		BTNode d = a.addNewLeaf("d");
+		BTNode e = b.addNewLeaf("e");
+//		BTNode a = root.addNewLeaf("a");
+//		BTNode b = a.addNewLeaf("b");
+//		BTNode c = b.addNewLeaf("c");
+//		BTNode d = c.addNewLeaf("d");
+//		BTNode e = d.addNewLeaf("e");
+		root.printTree();
+		System.out.println();
+		System.out.println();
+		e.setAsRoot();
+		e.printTree();
+		
+		System.out.println();
+		System.out.println();
+	}
+}
